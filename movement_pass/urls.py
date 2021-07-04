efcf56c9122bebe03888c176dc6d9f8a0cd4269f
mailzahidul <mailzahidul@gmail.com>
2021-07-04 22:25:21 +0600

    Login and Registration Complete

--- movement_pass/urls.py
+++ movement_pass/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.userlogin, name='login'),
+    path('account/login/', views.userlogin, name='login'),
+    path('account/logout/', views.userlogout, name='logout'),
     path('registration/', views.RegistrationView.as_view(), name='registration'),
 ]
--- movement_pass/views.py
+++ movement_pass/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import District, Gender, IdType, Registration
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 
-def home(request):
+def home(request):  
     return render(request, 'pages/home.html')
 
 
 def userlogin(request):
+    if request.method == 'POST':
+        phone = request.POST['phone']
+        date_of_birth = request.POST['date_of_birth']
+        user = authenticate(username=phone, password=date_of_birth)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Success")
+            return redirect('home')
+        else:
+            messages.danger(request, "Phone or Date of Birth invalid")
     return render(request, 'core/login.html')
 
+def userlogout(request):
+    logout(request)
+    return redirect('home')
 
 class RegistrationView(View):
 
@@ -40,19 +54,19 @@ class RegistrationView(View):
         id_number = request.POST['id_number']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        photo = request.POST.get('photo')
-        print("Before Password match")
+        photo = request.FILES.get('photo')
         if password1 != password2:
-            messages.danger(request, "Password not match...")
-        print("Before auth_info")
-        auth_info={
-            'username':phone,
-            'password':password1
-            }
-        new_user = User(**auth_info)
-        new_user.save()
-        print("Before data save")
-        regi = Registration(name=name, user=new_user, district=district_get, thana=thana, gender=gender_get, date_of_birth=date_of_birth, id_type=id_type_get, id_number=id_number, photo=photo)
-        regi.save()
-        messages.success(request, "Ragistration success")        
+            messages.error(request, "Password not match...")
+
+        has_account = Registration.objects.filter(user=phone)
+        if has_account:
+            messages.error(request, phone+" Already Exist.")
+        try:
+            new_user = User.objects.create_user(username=phone, password=date_of_birth)
+            regi = Registration(user=new_user, name=name, district=district_get, thana=thana, gender=gender_get, date_of_birth=date_of_birth, id_type=id_type_get, id_number=id_number, photo=photo)
+            regi.save()
+            messages.success(request, "Ragistration success")  
+        except Exception as errors:
+            messages.error(request, phone+" Already Exist.")
+            return redirect('registration')             
         return redirect('home')
